f794f9b62cb2545e95de2271e7d080c348e7b973
Giordi JoJo <jodi.fungula@gmail.com>
2022-05-26 20:04:46 +0200

    fixed updates

--- main.js
+++ main.js
@@ -1,62 +1,72 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let userChoice
-let computerChoice
-let result
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let userChoice;
+let computerChoice;
+let result;
+let example;
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
-}))
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+  })
+);
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * 3) + 1
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    if (randomNumber === 1) {
-        computerChoice = 'rock'
-    }
-    if (randomNumber === 2) {
-        computerChoice = 'paper'
-    }
-    if (randomNumber === 3) {
-        computerChoice = 'scissors'
-    }
-    computerChoiceDisplay.innerHTML = computerChoice
+  if (randomNumber === 1) {
+    computerChoice = 'rock';
+  }
+  if (randomNumber === 2) {
+    computerChoice = 'paper';
+  }
+  if (randomNumber === 3) {
+    computerChoice = 'scissors';
+  }
+  computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 function getResult() {
-    if (computerChoice === userChoice) {
-        result = 'its a draw!'
-        // result.style.color = 'brown'
-    }
-    if (computerChoice === 'rock' && userChoice === 'paper') {
-        result = 'You Win!'
-        // result.style.color = 'brown'
-    }
-    if (computerChoice === 'rock' && userChoice === 'scissors') {
-        result = 'You Lose!'
-        // result.style.color = 'brown'
-    }
-    if (computerChoice === 'paper' && userChoice === 'scissors') {
-        result = 'You Win!'
-        // result.style.color = 'brown'
-    }
-    if (computerChoice === 'paper' && userChoice === 'rock') {
-        result = 'You Lose!'
-        // result.style.color = 'brown'
-    }
-    if (computerChoice === 'scissors' && userChoice === 'paper') {
-        result = 'You Lose!'
-        // result.style.color = 'brown'
-    }
-    if (computerChoice === 'scissors' && userChoice === 'rock') {
-        result = 'You Win!'
-        // result.style.color = 'brown'
-    }
-    resultDisplay.innerHTML = result
-    // result.style.color = 'brown'
+  var result = document.getElementById('result');
+  let beforeStartGameText = 'You have not won the game yet';
+  //   beforeStartGameText.style.color = 'red';
+
+  if (computerChoice === userChoice) {
+    result = 'its a draw';
+  }
+
+  if (computerChoice === userChoice) {
+    result = 'its a draw!';
+    resultDisplay.style.color = 'brown';
+  }
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    result = 'You Win!';
+    resultDisplay.style.color = 'green';
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    result = 'You Lose!';
+    resultDisplay.style.color = 'red';
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    result = 'You Win!';
+    resultDisplay.style.color = 'pink';
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    result = 'You Lose!';
+    resultDisplay.style.color = 'blue';
+  }
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    result = 'You Lose!';
+    resultDisplay.style.color = 'brown';
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    result = 'You Win!';
+    resultDisplay.style.color = 'brown';
+  }
+  resultDisplay.innerHTML = result;
 }
